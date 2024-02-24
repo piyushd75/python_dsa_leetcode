b5b07b89792369f6e0a1769f629d18b59c59deb5
Piyush Diwaker <piyush@Piyushs-MacBook-Air.local>
2024-02-24 21:41:17 +0530

    feat: Add insert, remove, reverse method
    Implemented insert, remove, reverse functionality in Linked List

--- linkedlist.py
+++ linkedlist.py
@@ -80,10 +80,51 @@ class Linkedlist:
             return True
         return False
     
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next =temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length:
+            return self.pop()
+        pre = self.get(index-1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+        return True  
+    
         
 my_linked_list = Linkedlist(1)
 
-my_linked_list.append(2)
+my_linked_list.append(4)
 # my_linked_list.append(7)
 # my_linked_list.append(9)
 # my_linked_list.append(11)
@@ -112,6 +153,19 @@ my_linked_list.print_list()
 
 # print("--- get value by index ---")
 # print(my_linked_list.get(3))
-print(my_linked_list.set_value(0, 9))
+# print(my_linked_list.set_value(0, 9))
 print("--- after set value by index ---")
+my_linked_list.print_list()
+
+my_linked_list.insert(1, 2)
+my_linked_list.insert(2, 3)
+print("--- after insert value ---")
+my_linked_list.print_list()
+
+# my_linked_list.remove(2)
+# print("--- after remove value ---")
+# my_linked_list.print_list()
+
+my_linked_list.reverse()
+print("--- after reverse value ---")
 my_linked_list.print_list()
